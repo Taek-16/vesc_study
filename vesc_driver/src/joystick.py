a0a288c0a465b15dfbb360498562b03078a9eecb
JHPrk <qkrwogus1236@gmail.com>
2021-02-09 16:40:37 +0900

    joystick_add

---
+++ vesc_driver/src/joystick.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python
+#-*-coding:utf-8-*-
+
+import pygame
+from pygame.locals import *
+
+
+class joystick_handler(object):
+    def __init__(self, id):
+        self.id = id
+        self.joy = pygame.joystick.Joystick(id)
+        self.name = self.joy.get_name()
+        self.joy.init()
+        self.numaxes    = self.joy.get_numaxes()
+        self.numballs   = self.joy.get_numballs()
+        self.numbuttons = self.joy.get_numbuttons()
+        self.numhats    = self.joy.get_numhats()
+
+        self.axis = []
+        for i in range(self.numaxes):
+            self.axis.append(self.joy.get_axis(i))
+
+        self.ball = []
+        for i in range(self.numballs):
+            self.ball.append(self.joy.get_ball(i))
+
+        self.button = []
+        for i in range(self.numbuttons):
+            self.button.append(self.joy.get_button(i))
+
+        self.hat = []
+        for i in range(self.numhats):
+            self.hat.append(self.joy.get_hat(i))
+
+class joystick_manager(threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self)
+        pygame.init()
+        pygame.event.set_blocked((MOUSEMOTION, MOUSEBUTTONUP, MOUSEBUTTONDOWN))
+        self.joycount = pygame.joystick.get_count()
+        if self.joycount == 0:
+            print("No joystick manager detected!")
+            self.quit(1)
+        self.joy = []
+        for i in range(self.joycount):
+            self.joy.append(joystick_handler(i))
+        self.observers = []
+        self.isRun = True
+
+    def register_observer(self, observer):
+        self.observers.append(observer)
+
+    def notify_observer(self, event_type, key, value):
+        for observer in self.observers:
+            observer.notify(event_type, key, value)
+
+    def run(self, detector):
+        while self.isRun:
+            for event in [pygame.event.wait(), ] + pygame.event.get():
+                # QUIT             none
+                # ACTIVEEVENT      gain, state
+                # KEYDOWN          unicode, key, mod
+                # KEYUP            key, mod
+                # MOUSEMOTION      pos, rel, buttons
+                # MOUSEBUTTONUP    pos, button
+                # MOUSEBUTTONDOWN  pos, button
+                # JOYAXISMOTION    joy, axis, value
+                # JOYBALLMOTION    joy, ball, rel
+                # JOYHATMOTION     joy, hat, value
+                # JOYBUTTONUP      joy, button
+                # JOYBUTTONDOWN    joy, button
+                # VIDEORESIZE      size, w, h
+                # VIDEOEXPOSE      none
+                # USEREVENT        code
+                elif event.type == KEYDOWN and event.key in [K_ESCAPE, K_q]:
+                    self.quit()
+                elif event.type == JOYAXISMOTION: 
+                    self.notify_observer(event.type, event.axis, event.value)
+                elif event.type == JOYHATMOTION:
+                    self.notify_observer(event.type, event.hat, event.value)
+                elif event.type == JOYBUTTONUP:
+                    self.notify_observer(event.type, event.button, 0)
+                elif event.type == JOYBUTTONDOWN:
+                    self.notify_observer(event.type, event.button, 1)
+    
+    def quit(self):
+        self.isRun = False
---
+++ vesc_driver/src/joystick_control_node.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python
+#-*-coding:utf-8-*-
+
+from pygame.locals import *
+import rospy
+import os, sys
+sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
+
+# ros message
+from vesc_msg.msg import VescInput
+from joystick import joystick_manager
+import threading
+
+# JOYAXISMOTION, JOYHATMOTION, JOYBUTTONUP, JOYBUTTONDOWN
+# JOYAXISMOTION : axisno. 2,  brake, range : -1~1, 
+# axisno. 5, speed, range : -1~1,
+# axisno. 0, servo_position, -1~1 
+# JOYHAT : v[1] => duty_cycle , 1 : 0.1  increase, -1 : 0.1 decrease
+class joystick_vesc_msg_creator:
+    def __init__(self):
+        self.fbrake = 0.0
+        self.fspeed = 0.0
+        self.fservo_position = 0.0
+        self.fduty_cycle = 0.0
+        self.is_ready = False
+    def notify(self, event_type, key, value):
+        self.is_ready = True
+        if event_type == JOYAXISMOTION:
+            if key == 2 :
+                self.fbrake = self.__convert_brake(value)
+            elif key == 5 :
+                self.fspeed = self.__convert_speed(value)
+            elif key == 0 :
+                self.servo_position = self.__convert_servo(value)
+        elif event_type == JOYHATMOTION:
+            if key == 0 :
+                self.fduty_cycle += self.__convert_duty_cycle(value[1])
+
+    def __convert_speed(self, value):
+        return (value + 1) * 1000
+
+    def __convert_servo(self, value):
+        return (value + 1)/2
+
+    def __convert_duty_cycle(self, value):
+        return value * 0.1
+
+    def __convert_brake(self, value):
+        return (value + 1)*10
+
+    def get_send_message(self):
+        self.is_ready = False
+        return self.fbrake, self.fspeed, self.servo_position, self.duty_cycle
+
+    def msg_ready(self):
+        return self.is_ready
+
+def main():
+    rospy.init_node("control_node")
+    control_pub = rospy.Publisher("joystick_input", VescInput, queue_size=1)
+    joyManager = joystick_manager()
+    joyObserver = joystick_vesc_msg_creator()
+
+    joyManager.register_observer(joyObserver)
+
+    rate = rospy.Rate(20)
+    vesc_input = VescInput()
+    joyManager.Start()
+    while not rospy.is_shutdown():
+        while not joyObserver.msg_ready():
+            rate.sleep()
+            continue
+
+        vesc_input.write_brake, /
+        vesc_input.write_speed, /
+        vesc_input.write_servo_position, /
+        vesc_input.write_duty_cycle = joyObserver.get_send_message()
+        control_pub.publish(vesc_input)
+        rate.sleep()
